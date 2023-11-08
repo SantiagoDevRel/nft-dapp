3cf8eebe90aa31018a17835852ff6e949890ac4c
your_username <stztrujillo@hotmail.com>
2023-11-08 17:36:48 +0000

    edited artifacts path

--- nft-dapp/hardhat.config.js
+++ nft-dapp/hardhat.config.js
@@ -1,9 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "sepolia",
   solidity: "0.8.19",
   paths: {
     artifacts: "./src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      accounts: ["0x" + process.env.pk],
+      url: process.env.sepolia,
+    },
   },
 };
