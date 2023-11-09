8b83d7fcc703865942ed025eea7b06846ea4eca1
your_username <stztrujillo@hotmail.com>
2023-11-09 09:48:26 +0000

    added cost and totalSupply functions

--- nft-dapp/contracts/NFT.sol
+++ nft-dapp/contracts/NFT.sol
@@ -7,19 +7,35 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract NFT_CS is ERC721, Ownable {
 
     uint256 private s_totalSupply;
+    uint256 private s_cost;
 
     constructor()
         ERC721("NFT_CS", "CSAFE");
         Ownable(msg.sender);
+        s_cost = 0;
     {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://docs.gaming.chainsafe.io/img/logo.png";
     }
 
-    function safeMint() public {
+    function safeMint() public payable {
+        require(msg.value == cost(), "The value doesn't match the cost of the NFT");
         s_totalSupply++;
         uint256 memory _tokenId = s_totalSupply;
         _safeMint(msg.sender, _tokenId);
+    }
+
+    function changeCost(uint256 _cost) external onlyOwner {
+        require(_cost < 1 ether, "the NFT can't cost more than 1 ether");
+        s_cost = _cost;
+    }
+
+    function cost() public view returns(uint256){
+        return s_cost;
+    }
+
+    function totalSupply() public view returns(uint256){
+        return s_totalSupply;
     }
 }
