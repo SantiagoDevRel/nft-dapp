174d3be99903b528460a308c4c8f6fb3d447c739
your_username <stztrujillo@hotmail.com>
2023-11-09 09:29:13 +0000

    implemented basic erc721 with baseURI

--- nft-dapp/contracts/NFT.sol
+++ nft-dapp/contracts/NFT.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract NFT {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT_CS is ERC721, Ownable {
+
+    uint256 private s_totalSupply;
+
+    constructor()
+        ERC721("NFT_CS", "CSAFE");
+        Ownable(msg.sender);
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://docs.gaming.chainsafe.io/img/logo.png";
+    }
+
 }
